166255f44a8ecb80fca62c102cdcfbb8d4ff2807
fydar <fydar.dev@gmail.com>
2026-02-04 01:34:59 +0000

    ⚡ Strip all shader variants that aren't used in the project
    I'm not using a ShaderVariantCollection to list all Shader Variants that are used in the project and using a build time processor to remove all Shader variants that arne't used in the project. This cuts the build size down by ~0.5mb.

--- src/AstralElitesUnity/Assets/AstralElites/Engineering/Powerplate/ShaderStripper.cs
+++ src/AstralElitesUnity/Assets/AstralElites/Engineering/Powerplate/ShaderStripper.cs
@@ -1,20 +1,65 @@
 #if UNITY_EDITOR && UNITY_WEBGL
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ShaderStripper : IPreprocessShaders
 {
+    private const string AssetPath = "Assets/AstralElites/Gameplay/Effects/All Shaders.shadervariants";
+    private const string InternalGuiShader = "Hidden/Internal-GUITexture";
+
+    private ShaderVariantCollection _collection;
+
     public int callbackOrder => 0;
 
     public void OnProcessShader(Shader shader, ShaderSnippetData snippet, IList<ShaderCompilerData> data)
     {
-        if (shader.name == "Hidden/Light2D")
+        if (shader.name == InternalGuiShader)
         {
-            data.Clear();
             return;
         }
+
+        if (_collection == null)
+        {
+            _collection = AssetDatabase.LoadAssetAtPath<ShaderVariantCollection>(AssetPath);
+
+            if (_collection == null)
+            {
+                Debug.LogError($"[ShaderStripper] Failed to find ShaderVariantCollection at {AssetPath}. Build may fail or be incorrect.");
+                return;
+            }
+        }
+
+        for (int i = data.Count - 1; i >= 0; i--)
+        {
+            var variant = new ShaderVariantCollection.ShaderVariant
+            {
+                shader = shader,
+                passType = snippet.passType,
+                keywords = GetKeywordArray(data[i].shaderKeywordSet)
+            };
+
+            if (!_collection.Contains(variant))
+            {
+                data.RemoveAt(i);
+            }
+        }
+    }
+
+    private string[] GetKeywordArray(ShaderKeywordSet keywordSet)
+    {
+        var keywords = keywordSet.GetShaderKeywords();
+        string[] keywordNames = new string[keywords.Length];
+
+        for (int i = 0; i < keywords.Length; i++)
+        {
+            keywordNames[i] = keywords[i].name;
+        }
+
+        return keywordNames;
     }
 }
 #endif
