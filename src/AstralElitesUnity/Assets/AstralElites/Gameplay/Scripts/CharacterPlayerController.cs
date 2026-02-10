5b49bf08efe26552f74ebc34060ef2995d586a87
fydar <fydar.dev@gmail.com>
2026-02-10 17:50:51 +0000

    ✨ Updated input for better touch and controller support

--- src/AstralElitesUnity/Assets/AstralElites/Gameplay/Scripts/CharacterPlayerController.cs
+++ src/AstralElitesUnity/Assets/AstralElites/Gameplay/Scripts/CharacterPlayerController.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
 
 [RequireComponent(typeof(Character))]
 public class CharacterPlayerController : MonoBehaviour
 {
     [SerializeField] private InputActionReference analogMovementInput;
+    [SerializeField] private InputActionReference analogLookInput;
 
     private Character character;
     private Camera mainCamera;
@@ -15,6 +17,7 @@ public class CharacterPlayerController : MonoBehaviour
         mainCamera = Camera.main;
 
         analogMovementInput.asset.Enable();
+        analogLookInput.asset.Enable();
     }
 
     private void Update()
@@ -24,31 +27,83 @@ public class CharacterPlayerController : MonoBehaviour
             return;
         }
 
-        bool isUsingMouse = false;
-        if (Mouse.current != null)
+        Vector2 thrust = analogMovementInput.action.ReadValue<Vector2>();
+        Vector2 lookInput = analogLookInput.action.ReadValue<Vector2>();
+
+        bool isRotating = false;
+        float rotationTarget = character.inputRotation;
+        bool isThrusting = false;
+
+        if (lookInput.sqrMagnitude > 0.01f)
         {
-            var ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            isRotating = true;
+            rotationTarget = Mathf.Atan2(lookInput.y, lookInput.x) * Mathf.Rad2Deg;
+        }
+        else
+        {
+            Vector2 pointerPosition = Vector2.zero;
+            bool pointerFound = false;
 
-            var scenePoint = ray.origin + (ray.direction * 10);
-
-            float angleRadians = Mathf.Atan2(scenePoint.y - transform.position.y,
-                scenePoint.x - transform.position.x);
-
-            float angleDegrees = angleRadians * Mathf.Rad2Deg;
-
-            character.inputRotation = angleDegrees;
-
-            if (Mouse.current?.leftButton.isPressed ?? false)
+            if (Touchscreen.current != null)
             {
-                character.inputThrust = transform.right;
-                isUsingMouse = true;
+                var touchSum = Vector2.zero;
+                int activeCount = 0;
+
+                foreach (var touch in Touchscreen.current.touches)
+                {
+                    if (touch.press.isPressed)
+                    {
+                        touchSum += touch.position.ReadValue();
+                        activeCount++;
+                    }
+                }
+
+                if (activeCount > 0)
+                {
+                    pointerPosition = touchSum / activeCount;
+                    isThrusting = true;
+                    pointerFound = true;
+                }
+            }
+            
+            if (!pointerFound && Pen.current != null)
+            {
+                if (Pen.current.tip.isPressed)
+                {
+                    pointerPosition = Pen.current.position.ReadValue();
+                    isThrusting = true;
+                    pointerFound = true;
+                }
+            }
+            
+            if (!pointerFound && Mouse.current != null)
+            {
+                pointerPosition = Mouse.current.position.ReadValue();
+                isThrusting = Mouse.current.leftButton.isPressed;
+                pointerFound = true;
+            }
+
+            if (pointerFound)
+            {
+                var ray = mainCamera.ScreenPointToRay(pointerPosition);
+                var scenePoint = ray.origin + (ray.direction * 10);
+                Vector2 direction = (Vector2)scenePoint - (Vector2)transform.position;
+
+                isRotating = true;
+                rotationTarget = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            }
+
+            if (isThrusting)
+            {
+                thrust = (Vector2)transform.right;
             }
         }
 
-        if (!isUsingMouse)
+        if (isRotating)
         {
-            var input = analogMovementInput.action.ReadValue<Vector2>();
-            character.inputThrust = input;
+            character.inputRotation = rotationTarget;
         }
+
+        character.inputThrust = thrust;
     }
 }
